ec5e2f1acd87324fda751f9d716313649f582ae8
Oleksander Pyavchik <pyavchik@gmail.com>
2026-01-06 23:40:03 +0200

    Fix: Pass reset token to AuthDialog

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -396,6 +396,7 @@ export default function App() {
           loadUserData();
           loadCurrentUser();
         }}
+        initialResetToken={resetPasswordToken || undefined}
       />
 
       {/* Authentication UI */}
